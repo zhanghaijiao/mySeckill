9b12174bda421c59ef103a3a7de6677dbbe05b96
zhanghaijiao <164793081@qq.com>
2019-04-02 15:48:02 +0800

    数据库建表语句

--- src/main/sql/seckill.sql
+++ src/main/sql/seckill.sql
@@ -57,3 +57,19 @@ call execute_seckill(1001, 18801147595, now(), @r_result);
 --获取结果
 SELECT @r_result;
 
+
+
+--创建表语句
+CREATE TABLE seckill  (
+  `seckill_id` bigint NOT NULL AUTO_INCREMENT COMMENT '商品库存id',
+  `name` varchar(120) NOT NULL COMMENT '商品名称',
+  `number` int NOT NULL COMMENT '库存数量',
+  `start_time` timestamp NOT NULL  COMMENT '秒杀开启时间',
+  `end_time` timestamp NOT NULL  COMMENT '秒杀结束时间',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '记录创建时间',
+PRIMARY KEY (`seckill_id`),
+INDEX `idx_start_time`(`start_time`),
+INDEX `idx_end_time`(`end_time`),
+INDEX `idx_create_time`(`create_time`)
+) ENGINE = InnoDB AUTO_INCREMENT = 1000 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '秒杀库存表' ROW_FORMAT = Compact;
+
